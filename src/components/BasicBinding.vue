cd1a1045f646d5ac742f5c7e742a853e6a9edcde
Tamara S <stefanov.tamara@gmail.com>
2021-08-04 01:04:31 -0400

    add license

--- src/components/BasicBinding.vue
+++ src/components/BasicBinding.vue
@@ -3,12 +3,9 @@
     <h3>What goes into a binding</h3>
     <button @click="showMeMore">Show More</button>
 
-    <pre
-      :class="{ hidden: elShown < 3 }"
-      v-showGuts:test.something="'thebest'"
-    />
-    <pre :class="{ hidden: elShown < 2 }" v-showGuts:test.something />
-    <pre :class="{ hidden: elShown < 1 }" v-showGuts:test />
+    <pre v-if="elShown >= 3" v-showGuts:test.something="'thebest'" />
+    <pre v-if="elShown >= 2" v-showGuts:test.something />
+    <pre v-if="elShown >= 1" v-showGuts:test />
     <pre v-showGuts></pre>
   </div>
 </template>
