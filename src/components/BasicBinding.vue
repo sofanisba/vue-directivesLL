d51b80682bd29938516785007fb514f71f755941
Tamara Stefanovic <stefanov.tamara@gmail.com>
2021-08-04 00:52:05 -0400

    update for deployed app

--- src/components/BasicBinding.vue
+++ src/components/BasicBinding.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="basic-binding">
     <h3>What goes into a binding</h3>
+    <button @click="showMeMore">Show More</button>
+
+    <pre
+      :class="{ hidden: elShown < 3 }"
+      v-showGuts:test.something="'thebest'"
+    />
+    <pre :class="{ hidden: elShown < 2 }" v-showGuts:test.something />
+    <pre :class="{ hidden: elShown < 1 }" v-showGuts:test />
     <pre v-showGuts></pre>
-    <!-- <pre v-showGuts:test></pre> -->
-    <!-- <pre v-showGuts:test.something></pre> -->
-    <!-- <pre v-showGuts:test.something="'thebest'"></pre> -->
   </div>
 </template>
 <script>
 export default {
   name: "BasicBinding",
+  data() {
+    return {
+      elShown: 0,
+    };
+  },
+
   directives: {
     showGuts: {
       bind(el, binding) {
@@ -17,5 +28,16 @@ export default {
       },
     },
   },
+
+  methods: {
+    showMeMore() {
+      this.elShown++;
+    },
+  },
 };
 </script>
+<style scoped>
+.hidden {
+  display: none;
+}
+</style>
